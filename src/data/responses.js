280ea91eac3be61b8db2250b961659ff43a5fe3a
kamaci <furkankamaci@gmail.com>
2018-04-26 12:31:08 +0300

    Bot commands are updated.

--- src/data/responses.js
+++ src/data/responses.js
@@ -10,13 +10,48 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 const RESPONSES = {
-    'colendi':      'Colendi is a blockchain based decentralized credibility evaluation and microcredit platform, ' +
-                    'fully compatible with participation banking requirements as well as traditional financing ' +
-                    'methods. Through its decentralized structure that is built on machine learning algorithms, it ' +
-                    'gathers and processes all digital data of a user -from online sources to social media and ' +
-                    'smartphone data to accredited merchant shopping history- in order to create a universal digital ' +
-                    'identity and comprehensive credibility image.',
-    'help':         "Valid commands are: !colendi, !help"
+
+    'about':        'Good day Colendi Community. This telegram channel is designed to talk about the Colendi Project and the Colendi Token in detail. ' +
+                    'You may use our Telegram bot by pressing " / " and selecting from the options.',
+
+    'rules':        '"We\'d like to remind you of our Telegram rules.\n' +
+                    '\n' +
+                    '- Please use English in all your communications at all times.\n' +
+                    '- Please refrain from offensive language under any circumstance.\n' +
+                    '- Please stick to Colendi related topics.\n' +
+                    '- Please do not promote any other token, project, company, event, telegram channel etc. in this group. Official partner announcements or mentions will be made by the admins.\n' +
+                    '- Speculative language is strictly forbidden. Please refrain from using manipulative phrases including the crypto-slang such as ""moon, lambo, hodl"". These messages will be deleted immediately.\n' +
+                    '- Bear in mind that all Colendi team members have an admin tag and please be careful with any other person who may try to contact you. ' +
+                        'We do not use the name of Colendi Project to ask for money, channel traffic, tokens etc., so please pay attention to whom you might be messaging with.\n' +
+                    '- Feel free to contact us when you have a question, a doubt or just a happy thought about Colendi :)\n' +
+                    'For further information, you may read the pinned message, visit our blog page and website:\n' +
+                    'www.colendi.com\n' +
+                    'www.colendi.com/blog\n' +
+                    'Feel free to ask us anything. Thank you for your attention and support!"',
+
+    'colendi':      'Colendi is a decentralized credit scoring platform based on the blockchain. It uses its patented machine learning algorithms to generate a score that will serve as a financial passport for its users. ' +
+                    'These users, therefore, may have access to some advantages such as microcredit, installment shopping or p2p lending. ' +
+                    'Colendi is specially designed to serve to unbanked or underbanked as well as being perfectly capable of adapting to traditional or participation banking methods.',
+
+    'team':         'A team of 20 engineers and business professionals, truly passionate about blockchain and the fintech sector comprises the Colendi team. ' +
+                    'The team\'s previous ventures including companies like Ininal, Provus, Lagom, Marvin, Fintechtime proved to be successful exits reaching out to a more than a million unbanked users along with the other customers in the local market. ' +
+                    'The team grew its area of expertise further by adding the machine learning algorithms and blockchain know-how in order to create Colendi Protocol on ethereum blockchain. ' +
+                    'To find out more about the profiles of the team please visit www.colendi.com',
+
+    'token':        'Colendi Token serves for two utilities.' +
+                    '\n1)The means of exchange for credit score queries by merchants to issue microcredit or installment shopping to their customers.' +
+                    '\n2) The token will be staked by lenders and data integrating parties. Smart contracts will include a reward and punishment mechanism as an auditing mechanism that will incentivize all parties to provide quality data.',
+
+    'community':    'To Colendi, the community is of vital importance. Colendi reaches out to its community to understand their needs and interact with it to better explain the Colendi Project. ' +
+                    'Colendi also has a bounty campaign where the community members with the right skills can contribute to the project and get rewarded for their work. ' +
+                    'You may visit our bounty campaign at https://bitcointalk.org/index.php?topic=3301333',
+
+    'application':  'Colendi Application will serve its customers with 3 main functions that are self-sovereign digital ID, global credit score and a microcredit platform that helps lenders and borrowers facilitate loans. ' +
+                    'A native wallet service will be ready in the Colendi App to enable users to manage their funds.',
+
+    'project':      'The Colendi Project is designed to give a Colendi Score to its user which would be global, borderless, mobile, well-rounded, and objective, as well as being adaptive to lender’s preferences. ' +
+                    'The Colendi Score does not rely on centralized and monopolistic scoring systems and enables real-time credit decision making. ' +
+                    'To find out more in detail you may read our whitepaper: https://www.colendi.com/whitepaper'
 };
 
 module.exports = RESPONSES;
